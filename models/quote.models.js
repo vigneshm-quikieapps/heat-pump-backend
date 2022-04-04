c3428dae4f93c770c34fbd8ec43715a51db51f0d
Siddharth Kumar Yadav <siddharth@quikieapps.com>
2022-04-04 23:23:05 +0530

    added quote & fabric models

---
+++ models/quote.models.js
@@ -0,0 +1,104 @@
+/**
+ * @author Siddharth_Kumar_Yadav
+ * @Since 07 Mar 2022
+ */
+
+ const mongoose = require("mongoose"),
+ Schema = mongoose.Schema;
+
+/**
+* User schema
+*/
+const QuoteSchema = new mongoose.Schema(
+  { 
+      site_details:[{ address_1: {
+        type: String,
+      },
+      address_2: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      postcode: {
+        type: String,
+      }}],
+      occupancy:[{
+        
+      }],
+      equipments:{
+          tvs:Number,
+          laptops:Number,
+          Monitors:Number,
+          itServers:Number,
+          PhotoCopiers:Number
+      },
+      high_energy_equipments:{
+          sauna:Number,
+          swimmingPool:Number,
+          hotTub:Number,
+          kilns:Number,
+          other:Number
+      },
+    questions:{
+        hotwater_importance:Number,
+        woodStove_importance:Number,
+        electricity_than_uk_average:Number,
+    },
+
+    drawings:{
+        plans:[{
+            type:String
+        }],
+        elevations:[{
+            type:String
+        }],
+        sections:[{
+            type:String
+        }]
+    },
+
+    photos:{
+        walls:[{
+            type:String
+        }],
+        roof:[{
+            type:String
+        }],
+        windows:[
+            {
+                type:String
+            }
+        ],
+        existing_boiler:[
+            {
+                type:String
+            }
+        ],
+        existing_radiator:[
+            {
+                type:String
+            }
+        ],
+        pipework:[
+            {
+                type:String
+            }
+        ]
+    },
+    
+    raditator_size:String,
+    window_size:String,
+    heating_system:{
+        type:Number
+    }
+},
+ { timestamps: true }
+    
+);
+
+
+module.exports = mongoose.model("Quote", QuoteSchema);
