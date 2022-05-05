550f31df9bff4e9d81ae467a3338d24afc9854b6
Siddharth Kumar Yadav <siddharth@quikieapps.com>
2022-05-05 15:51:00 +0530

    fix

--- server.js
+++ server.js
@@ -68,7 +68,8 @@ app.get("/swagger.json", function (req, res) {
 });
 
 const corsOpts = {
-  origin: "*",
+  origin: true,
+  credentials:true,
   methods: ["GET", "POST", "PATCH","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
