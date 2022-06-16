1276e946202efc9d665c82ef2c046b94801f7a48
Rajugopal1 <rajugopalsinghh@gmail.com>
2022-06-16 12:18:13 +0530

    search by customerName and sitedetails in gtallQte

--- controllers/quote/quoteController.js
+++ controllers/quote/quoteController.js
@@ -13,15 +13,15 @@ const quoteModels = require("../../models/quote.models");
 const { default: mongoose } = require("mongoose");
 const UserSchema = require("../../models/users.model");
 
-exports.getQuote=async (req,res,next)=>{
+exports.getQuote = async (req, res, next) => {
   var { qid } = req.query;
- 
+
   try {
     console.log(qid);
     const response = await quoteModels.findById(qid);
     res.json({
       success: true,
-      data: response
+      data: response,
     });
   } catch (err) {
     res.json({
@@ -29,20 +29,21 @@ exports.getQuote=async (req,res,next)=>{
       message: err.toString(),
     });
   }
+};
 
-}
+exports.getAllQuote = async (req, res, next) => {
+  var {
+    page,
+    perPage,
+    status,
+    cst = false,
+    siteDetails,
+    customerName,
+  } = req.query;
 
-exports.getAllQuote=async (req,res,next)=>{
-
-
-  var { page, perPage, status,cst=false , siteDetails, customerName} = req.query;
-
-
-//  console.log("CST",cst);
+  //  console.log("CST",cst);
 
   const userId = req.decodedAccessToken.id;
-
-
 
   if (!page) {
     page = 1;
@@ -50,42 +51,55 @@ exports.getAllQuote=async (req,res,next)=>{
   if (!perPage) {
     perPage = 10;
   }
-  
-  var customerSide=undefined;
-  if(cst){
-    customerSide=mongoose.Types.ObjectId(userId)
+
+  var customerSide = undefined;
+  if (cst) {
+    customerSide = mongoose.Types.ObjectId(userId);
   }
 
-  var filter={
+  var filter = {
     status: status || !null,
   };
 
-  if(cst){
-   filter.creator_customer_id=customerSide
+  if (cst) {
+    filter.creator_customer_id = customerSide;
   }
 
-
   try {
+    let response = await quoteModels
+      .find({
+        filter,
+        ...(siteDetails && {
+          "site_details.address_1": new RegExp(siteDetails, "i"),
+        }),
+      })
+      .sort({ createdAt: -1, updatedAT: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate("creator_customer_id","" , UserSchema);
+      
+      if(customerName){
+        response = response.filter(item => {
+          return item.creator_customer_id.name.toLowerCase().includes(customerName.toLowerCase())
+        }
+        );
+      };
+      
+      
 
-    const response = await quoteModels.find({
-      filter,
-      ...(siteDetails && {site_details: new RegExp(siteDetails, "i")}),
-      ...(customerName && {customer_name: new RegExp(customerName, "i")}),
-    })
-    .sort({createdAt: -1, updatedAT : -1 })
-    .skip((page - 1) * perPage)
-    .limit(perPage)
-    .populate("creator_customer_id","" , UserSchema);
-
-    const total_records = await quoteModels.find(filter).sort({createdAt: -1, updatedAT : -1 }).countDocuments();
+    const total_records = await quoteModels
+      .find(filter)
+      .sort({ createdAt: -1, updatedAT: -1 })
+      .countDocuments();
     const total_pages = Math.ceil(total_records / perPage);
-  console.log(response, response.length);
+    console.log(response, response.length);
     // console.log("LENGTH",response.length);
     res.json({
       success: true,
       message: "OK",
       current_page: page,
       total_records: total_records,
+      currentRecords: response.length,
       total_pages: total_pages,
       data: response,
     });
@@ -97,16 +111,14 @@ exports.getAllQuote=async (req,res,next)=>{
       total_pages: total_pages,
       message: err.toString(),
     });
-}
-}
-
-
+  }
+};
 
 exports.createQuote = async (req, res, next) => {
   // create a Quote
 
   const userId = req.decodedAccessToken.id;
-  console.log("ID",userId);
+  console.log("ID", userId);
   // var obj = ({
   //   site_details,
   //   occupancuy,
@@ -130,13 +142,12 @@ exports.createQuote = async (req, res, next) => {
 
   const time = new Date().getTime();
   const id = reversedNum(time);
-  const quote_reference_number =
-    "JR" + parseInt(id + Math.random() * 100);
+  const quote_reference_number = "JR" + parseInt(id + Math.random() * 100);
   obj.quote_reference_number = quote_reference_number;
 
   try {
-    obj.creator_customer_id=userId
-    console.log("OBJ",obj)
+    obj.creator_customer_id = userId;
+    console.log("OBJ", obj);
     const newQuote = new quoteModels(obj);
     const response = await newQuote.save();
     res.json({
@@ -153,7 +164,7 @@ exports.createQuote = async (req, res, next) => {
 };
 
 exports.patchQuote = async (req, res, next) => {
-  const id  = req.params.id;
+  const id = req.params.id;
   var obj = ({
     site_details,
     occupancuy,
@@ -179,7 +190,7 @@ exports.patchQuote = async (req, res, next) => {
     res.json({
       success: true,
       message: "UPDATED",
-      data: obj
+      data: obj,
     });
   } catch (err) {
     res.json({
@@ -194,36 +205,36 @@ module.exports.getQuoteStatus = async (req, res, next) => {
   try {
     const response = await quoteModels.find();
 
-  //   const response = await UserModel.find({ admin: false });
-  // const sArray = response;
+    //   const response = await UserModel.find({ admin: false });
+    // const sArray = response;
 
-  let newUnpaid = 0,
+    let newUnpaid = 0,
       newPaid = 0,
-     inprogress = 0,
-     complete = 0,
-     snagging = 0 ;
-  let total_records = response.length;
-  for (let i = 0; i < response.length; i++) {
-    switch (response[i].status) {
-      case 1:
-        newUnpaid += 1;
-        break;
-      case 2:
-        newPaid += 1;
-        break;
-      case 3:
-        inprogress += 1;
-        break;
-      case 3:
-        complete += 1;
-        break;
-      case 3:
-        snagging += 1;
-        break;
-      default:
-        total_records -= 1;
+      inprogress = 0,
+      complete = 0,
+      snagging = 0;
+    let total_records = response.length;
+    for (let i = 0; i < response.length; i++) {
+      switch (response[i].status) {
+        case 1:
+          newUnpaid += 1;
+          break;
+        case 2:
+          newPaid += 1;
+          break;
+        case 3:
+          inprogress += 1;
+          break;
+        case 3:
+          complete += 1;
+          break;
+        case 3:
+          snagging += 1;
+          break;
+        default:
+          total_records -= 1;
+      }
     }
-  }
     res.json({
       success: true,
       message: "OK",
@@ -236,11 +247,10 @@ module.exports.getQuoteStatus = async (req, res, next) => {
         total_records: total_records,
       },
     });
-  }
-  catch (err) {
+  } catch (err) {
     res.json({
       success: false,
       message: err.toString(),
     });
   }
-}
+};
