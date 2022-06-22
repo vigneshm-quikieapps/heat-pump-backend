05e74113a73595d626ab678c0b5f7b29c6059913
Rajugopal1 <rajugopalsinghh@gmail.com>
2022-06-22 15:03:17 +0530

    file added

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@
 
 require("dotenv").config();
 var dbConn =
-  "mongodb+srv://heatpump:tfb30fbNOWxf6Xld@cluster0.t8ibh.mongodb.net/heatpump?retryWrites=true&w=majority";
+  "mongodb+srv://heatpump:tfb30fbNOWxf6Xld@cluster0.t8ibh.mongodb.net/Heat_pump_Prod?retryWrites=true&w=majority";
 
 module.exports = {
   secret: "expressapitest",
